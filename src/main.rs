b539d7cee3a50d540a690f9df6f9952d2f171a50
Omar <ohyjek@gmail.com>
2022-06-15 16:33:07 -0400

    use tokio::spawn

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,36 @@
-use tokio::{net::TcpListener, io::{/*AsyncReadExt, */ AsyncWriteExt, BufReader, AsyncBufReadExt}};
+use tokio::{net::TcpListener, io::{AsyncWriteExt, BufReader, AsyncBufReadExt}};
 
-//create tcp echo server
 #[tokio::main]
 async fn main() { 
-    //Create TcpListener with addr; await and unwrap to get the value
+    //Create new TcpListener
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
-
-    //Tcp connection accept exposes socket: TcpStream and addr: SocketAddr
-    let (mut socket, _addr) = listener.accept().await.unwrap();
-
-    //Use Reader
-    let (reader, mut writer) = socket.split();
-
-    //Create memory buffer to read/store the socket data
-    let mut reader = BufReader::new(reader);
-    //need string for each line
-    let mut line = String::new();
-
-    loop {        
-    
-        //Read and return count of how many bytes we read
-        let bytes_read = reader.read_line(&mut line).await.unwrap();
-        if bytes_read == 0 {
-            break;
-        }
-    
-        //Write all of bytes from the read in buffer back to the socket
-        writer.write_all(line.as_bytes()).await.unwrap();
-        line.clear();
+    println!("Starting...");
+    loop {
+        //Await incoming connection
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+        println!("Incoming Connection...");
+        //Spawn new async task
+        tokio::spawn(async move {
+            println!("Spawning...");
+            //Deconstruct Reader and Writer from socket.split()
+            let (reader, mut writer) = socket.split();
+            //Create memory buffer to read/store the socket data
+            let mut line = String::new();
+            //Create mutable BufReader helper
+            let mut reader = BufReader::new(reader);
+        
+            //Read and write all the lines
+            loop {        
+            
+                //Read and return count of how many bytes we read
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+                if bytes_read == 0 {
+                    break;
+                }
+                //Write all of bytes from the read in buffer back to the socket
+                writer.write_all(line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
     }
-
 }
