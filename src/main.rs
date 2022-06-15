5eae1ce28020305474a4cd91e726b173dc873e1a
Omar <ohyjek@gmail.com>
2022-06-15 15:55:23 -0400

    before loop

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
-#[tokio::main] //takes async main function and turns it into a tokio main function
-async fn main() {//change to async 
-    println!("Hello, world!");
+use tokio::{net::TcpListener, io::{AsyncReadExt, AsyncWriteExt}};
+
+//create tcp echo server
+#[tokio::main]
+async fn main() { 
+    //Create TcpListener with addr; await and unwrap to get the value
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    //Tcp connection accept exposes socket: TcpStream and addr: SocketAddr
+    let (mut socket, _addr) = listener.accept().await.unwrap();
+
+    //Create memory buffer to read/store the socket data
+    let mut buffer = [0u8; 1024];
+
+    //Read and return count of how many bytes we read
+    let bytes_read = socket.read(&mut buffer).await.unwrap();
+    
+    //Write all of bytes from the buffer up to the bytes read to the socket
+    socket.write_all(&buffer[..bytes_read]).await.unwrap(); //not to every socket, writes all from input buffer to output buffer
 }
